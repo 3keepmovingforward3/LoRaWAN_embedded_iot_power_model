e36b7b1344661ac36719199d98895ab0a6893803
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2020-03-26 16:16:27 -0400

    switch to 5 days of darkness

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import math
 import node
 import random as random
 
@@ -12,22 +11,12 @@ import random as random
 # Returns a float multiplied by the specified power of 10.
 # Statistical p-value = 0.05
 #
-battery_capacity = (1.0 - random.random() * 0.05)*6600e-3
+battery_capacity = 7200
 
-sleep = node.State("Sleep", 8e-3, 8e-3)
-listen = node.State("Wakeup & Listen", 500e-3, 1000e-3)
-camera = node.State("Camera Wakeup & Image Recognition", 1100e-3, 2200e-3)
-alert = node.State("Alert", 1260e-3, 25320e-3)
-
-
-#  Location: Eureka, California
-# a = amplitude
-#
-def charge(t):
-    a = 0.5*(14.0 + 55.0/60.0)
-    c = 0.5*(14.0 - 55.0/60.0)
-
-    return (a * math.cos(2.0 * t * math.pi / 365.0) + c) * 500e-3
+sleep = node.State("Sleep", 8, 8)
+listen = node.State("Wakeup & Listen", 500, 1000)
+camera = node.State("Camera Wakeup & Image Recognition", 1100, 2200)
+alert = node.State("Alert", 1260, 25320)
 
 
 def main():
@@ -36,8 +25,8 @@ def main():
     #  Day 1 = Summer Solstice
     #  Normalized to battery
     #
-    print(str(((battery_capacity+charge(365)) -
-               ((0.9*sleep.qp*24) + (0.1*listen.qp)*24)))+"mAh after 24 hours")  # alpha simulation
+    lit = battery_capacity - ((0.9*sleep.qp*24*5) + (0.1*listen.qp)*24*5)  # alpha simulation
+    print(str(lit)+' mAh')
 
 
 if __name__ == '__main__':
