2a3427619feb08b24762a9337250a053dbed9e5d
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2020-03-26 15:39:21 -0400

    float all values remove format TBD format

--- main.py
+++ main.py
@@ -12,28 +12,32 @@ import random as random
 # Returns a float multiplied by the specified power of 10.
 # Statistical p-value = 0.05
 #
-battery_capacity = (1 - random.random() * 0.05)*6600e-6
+battery_capacity = (1.0 - random.random() * 0.05)*6600e-3
 
-sleep = node.State("Sleep", 8e-6, 8e-6)
-listen = node.State("Wakeup & Listen", 500e-6, 1000e-6)
-camera = node.State("Camera Wakeup & Image Recognition", 1100e-6, 2200e-9)
-alert = node.State("Alert", 1260e-6, 25320e-6)
+sleep = node.State("Sleep", 8e-3, 8e-3)
+listen = node.State("Wakeup & Listen", 500e-3, 1000e-3)
+camera = node.State("Camera Wakeup & Image Recognition", 1100e-3, 2200e-3)
+alert = node.State("Alert", 1260e-3, 25320e-3)
 
 
-#  Location: Rohnert Park, California
+#  Location: Eureka, California
+# a = amplitude
 #
 def charge(t):
-    return (2.675 * math.cos(2 * t * math.pi / 365) + 12.175) * 500e-6
+    a = 0.5*(14.0 + 55.0/60.0)
+    c = 0.5*(14.0 - 55.0/60.0)
+
+    return (a * math.cos(2.0 * t * math.pi / 365.0) + c) * 500e-3
 
 
 def main():
 
-    #  Day 364 = Winter Solstice
+    #  Day 365 = Winter Solstice
     #  Day 1 = Summer Solstice
     #  Normalized to battery
     #
-    print("{:.3e}".format(((battery_capacity+charge(364)) -
-                           ((0.9*sleep.qp*24) + (0.1*listen.qp)*24)))+"mAh after 24 hours")  # alpha simulation
+    print(str(((battery_capacity+charge(365)) -
+               ((0.9*sleep.qp*24) + (0.1*listen.qp)*24)))+"mAh after 24 hours")  # alpha simulation
 
 
 if __name__ == '__main__':
